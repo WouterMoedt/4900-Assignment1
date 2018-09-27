28d1d9c88472d112b1d173a32e6a9f67ef3ace66
WouterMoedt <wouter.moedt@live.nl>
2018-09-26 20:53:20 -0500

    Test

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,11 @@ from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status='published')
+        return super(PublishedManager,
+                     self).get_queryset() .filter(status='published')
 
 
-class Post(models.Model):
+class Post (models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
@@ -27,15 +28,8 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
-
-    objects = models.Manager()  # The default manager.
-    published = PublishedManager()  # Our custom manager.
-
-    class Meta:
-        ordering = ('-publish',)
-
-    def __str__(self):
-        return self.title
+    objects = models.Manager()      # The default manager.
+    published = PublishedManager()  # Custom manager.
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
@@ -43,3 +37,9 @@ class Post(models.Model):
                              self.publish.month,
                              self.publish.day,
                              self.slug])
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
